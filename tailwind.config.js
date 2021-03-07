c9a0e838f449391cd11c4ea62aa7616b6141b9dd
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-07 13:02:05 +0100

    footer tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,9 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    minHeight: {
-      '0': '0',
-      '1/4': '25%',
-      '1/2': '50%',
-      '3/4': '75%',
-      'full': '100%',
-     }
+    extend: {}
+    
+   
   },
   variants: {
     extend: {},
