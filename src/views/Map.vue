6fd4543ae2f0dd0ed61e9ece531d88b61f4dd30c
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-08 19:55:14 +0100

    first try maps - not good

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="h-4/5"> Map</div>
-
+  <div class="h-4/5">Map</div>
+  <div id="overviewMap"></div>
 </template>
 
 <script>
-export default {
+  export default {};
 
-}
+  
 </script>
 
-<style>
-
-</style>
+<style></style>
