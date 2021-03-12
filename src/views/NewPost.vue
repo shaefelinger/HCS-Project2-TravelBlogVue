8d7aebb4866243750f8242b079ad5125a4f118d8
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-12 08:06:41 +0100

    auth working locally

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,13 +1,16 @@
 <template>
-      <Banner :bannerImage="bannerImage" :bannerText="bannerText" :bannerButtonText="bannerButtonText" :bannerButtonLink="bannerButtonLink" />
+  <Banner :bannerImage="bannerImage" :bannerText="bannerText" :bannerButtonText="bannerButtonText" :bannerButtonLink="bannerButtonLink" />
   New Post
+  {{ status }}
+  <!-- <p>Name: {{ user.name }}</p> -->
 </template>
-
-
 
 <script>
   import Banner from '@/components/Banner.vue';
-  import bannerImage from '@/assets/banner2.jpg'
+  import bannerImage from '@/assets/banner2.jpg';
+
+  import axios from 'axios';
+  import router from '../router';
 
   export default {
     name: 'NewPost',
@@ -20,12 +23,44 @@
         bannerImage,
         bannerText: 'Add new Post...',
         bannerButtonText: 'Back',
-        bannerButtonLink: 'Home'
+        bannerButtonLink: 'Home',
+
+        status: 'unklar',
+        user: {
+          name: 'no user',
+        },
       };
+    },
+
+    methods: {
+      getUserData: function() {
+        let self = this;
+        // const tempURL = 'http://localhost:3000';
+        const tempURL = 'https://aroundtheworld-blog-server.herokuapp.com/';
+        ç;
+        // const tempURL = '';
+
+        axios
+          .get('auth/user')
+          .then((response) => {
+            console.log(this.user);
+            console.log(response.data.user);
+            this.user = response.data.user;
+            console.log('success 😎');
+            this.status = 'drin';
+            // self.$set(this, 'user', response.data.user);
+          })
+          .catch((errors) => {
+            console.log(errors);
+            this.status = 'nööö';
+            // router.push('/');
+          });
+      },
+    },
+    mounted() {
+      this.getUserData();
     },
   };
 </script>
 
-<style>
-
-</style>
+<style></style>
