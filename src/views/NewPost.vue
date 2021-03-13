3bf72d23fc769a05fc507b4c546e675167c4d2a3
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-13 12:17:54 +0100

    some small changes

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -2,7 +2,7 @@
   <Banner :bannerImage="bannerImage" :bannerText="bannerText" :bannerButtonText="bannerButtonText" :bannerButtonLink="bannerButtonLink" />
   New Post
   {{ status }}
-  <!-- <p>Name: {{ user.name }}</p> -->
+  <p>Name: {{ user.name }}</p>
 </template>
 
 <script>
@@ -10,7 +10,7 @@
   import bannerImage from '@/assets/banner2.jpg';
 
   import axios from 'axios';
-  // import router from '../router';
+  import router from '../router';
 
   export default {
     name: 'NewPost',
@@ -19,7 +19,6 @@
     },
     data() {
       return {
-        // bannerImage: '@/assets/banner2.jpg',
         bannerImage,
         bannerText: 'Add new Post...',
         bannerButtonText: 'Back',
@@ -32,33 +31,27 @@
       };
     },
 
-    // methods: {
-    //   getUserData: function() {
-    //     let self = this;
-    //     // const tempURL = 'http://localhost:3000';
-    //     // const tempURL = 'https://aroundtheworld-blog-server.herokuapp.com/';
-    //     // const tempURL = '';
+    methods: {
+      getUserData: function() {
+        let self = this;
 
-    //     axios
-    //       .get('auth/user')
-    //       .then((response) => {
-    //         console.log(this.user);
-    //         // console.log(response.data.user);
-    //         this.user = response.data.user;
-    //         // console.log('success 😎');
-    //         this.status = 'drin';
-    //         // self.$set(this, 'user', response.data.user);
-    //       })
-    //       .catch((errors) => {
-    //         console.log(errors);
-    //         this.status = 'nööö';
-    //         // router.push('/');
-    //       });
-    //   },
-    // },
-    // mounted() {
-    //   this.getUserData();
-    // },
+        axios
+          .get('auth/user')
+          .then((response) => {
+            this.user = response.data.user;
+            this.status = 'drin';
+            // self.$set(this, 'user', response.data.user);
+          })
+          .catch((errors) => {
+            console.log(errors);
+            this.status = 'nööö';
+            router.push('/');
+          });
+      },
+    },
+    mounted() {
+      this.getUserData();
+    },
   };
 </script>
 
