0f705adb0c4e2691767448cea4baf3720d1275d4
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-17 10:49:08 +0100

    SignIn nicer

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,14 +1,10 @@
 <template>
   <h1 class="mt-20 text-5xl text-center text-gray-500">Sign In-Page is coming soon...</h1>
-  <img class="w-96 m-auto" src="@/assets/underconstruction.jpg" alt="">
+  <img class="w-96 m-auto mt-10" src="@/assets/underconstruction.jpg" alt="">
+  <div class="text-center text-gray-600">
+      <h3 class="text-xl mt-6">Until then:</h3>
+      <p>Login: testuser@test.com</p>
+      <p>Password: test123</p>
+  </div>
 </template>
 
-<script>
-export default {
-
-}
-</script>
-
-<style>
-
-</style>
