e8fa5e8d8dc8855ce0fbc742fbd99a096a7a039f
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-18 09:09:01 +0100

    basic autocomplete working

--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -3,69 +3,73 @@
 
   <!-- <p>Name: {{ user }}</p> -->
 
-  <div class="formContainer">
-    <form id="addPostForm">
-      <label for="locationField" id="locationLabel">Enter a Location*</label>
-      <!-- <input ref="searchTextField" id="searchTextField" name="locationField" type="text" size="50"  autocomplete="on" /> -->
-      <input  id="searchTextField" type="text" />
+  <div class="formContainer ">
+    <div class="max-w-screen-sm px-8">
+      <form @submit.prevent="handleLocationSubmit">
+        <label for="locationField" id="locationLabel">Step 1: Enter a Location*</label>
+        <!-- <input ref="searchTextField" id="searchTextField" name="locationField" type="text" size="50"  autocomplete="on" /> -->
+        <input id="searchTextField" ref="searchTextField" type="search" name="locationField" />
+        <button>Submit Location</button>
+      </form>
+        <p>{{name}}</p>
+      <hr />
+      <form >
+        <label for="titleField">lat</label>
+        <input disabled v-model="lat" class="w-28" type="number" />
+        <label for="titleField">lng</label>
+        <input v-model="lng" type="number" class="w-28" />
 
-      <hr id="searchTextUnderline" class="" />
+        <label for="titleField">Enter a Title for the post*</label>
+        <input required type="text" v-model="title" name="titleField" id="titleField" />
 
-      <label  for="titleField">lat</label>
-      <input v-model="lat" class="w-28" type="number"  />
-      <label for="titleField">lng</label>
-      <input v-model="lng" type="number" class="w-28"  />
+        <label for="monthInput">Enter the date of your trip</label>
+        <div class="dateContainer">
+          <select required v-model="month" name="monthInput" id="monthField">
+            <option value="January">January</option>
+            <option value="February">February</option>
+            <option value="March">March</option>
+            <option value="April">April</option>
+            <option value="May">May</option>
+            <option value="June">June</option>
+            <option value="July">July</option>
+            <option value="August">August</option>
+            <option value="September">September</option>
+            <option value="Oktober">Oktober</option>
+            <option value="November">November</option>
+            <option value="December">December</option>
+          </select>
+          <input required v-model="year" class="yearInput" type="number" placeholder="Enter the year" min="1900" max="2100" id="yearField" />
+        </div>
 
-      <label for="titleField">Enter a Title for the post*</label>
-      <input required type="text" v-model="title" name="titleField" id="titleField" />
-
-      <label for="monthInput">Enter the date of your trip</label>
-      <div class="dateContainer">
-        <select required v-model="month" name="monthInput" id="monthField">
-          <option value="January">January</option>
-          <option value="February">February</option>
-          <option value="March">March</option>
-          <option value="April">April</option>
-          <option value="May">May</option>
-          <option value="June">June</option>
-          <option value="July">July</option>
-          <option value="August">August</option>
-          <option value="September">September</option>
-          <option value="Oktober">Oktober</option>
-          <option value="November">November</option>
-          <option value="December">December</option>
+        <label for="rating">Rate your trip </label>
+        <select v-model="rating" name="rating" id="ratingField">
+          <option value="0" disabled="disabled" selected="selected">Enter Rating</option>
+          <option class="star" value="1">★</option>
+          <option value="2">★★</option>
+          <option value="3">★★★</option>
+          <option value="4">★★★★</option>
+          <option value="5">★★★★★</option>
         </select>
-        <input required v-model="year" class="yearInput" type="number" placeholder="Enter the year" min="1900" max="2100" id="yearField" />
-      </div>
 
-      <label for="rating">Rate your trip </label>
-      <select v-model="rating" name="rating" id="ratingField">
-        <option value="0" disabled="disabled" selected="selected">Enter Rating</option>
-        <option class="star" value="1">★</option>
-        <option value="2">★★</option>
-        <option value="3">★★★</option>
-        <option value="4">★★★★</option>
-        <option value="5">★★★★★</option>
-      </select>
+        <label for="descriptionField">Enter a description</label>
+        <textarea v-model="description" id="descriptionField" name="descriptionField" rows="6" cols="80" placeholder=""></textarea>
 
-      <label for="descriptionField">Enter a description</label>
-      <textarea v-model="description" id="descriptionField" name="descriptionField" rows="6" cols="80" placeholder=""></textarea>
+        <label for="wikiField">Edit Wikipedia Information</label>
+        <textarea id="wikiField" name="wikiField" rows="6" cols="80" v-model="wiki"></textarea>
+        <p class="miniText">* = required</p>
 
-      <label for="wikiField">Edit Wikipedia Information</label>
-      <textarea id="wikiField" name="wikiField" rows="6" cols="80" v-model="wiki"></textarea>
-      <p class="miniText">* = required</p>
-
-      <div class="buttonContainer">
-        <button class="primaryButton" id="submitButton" @click.prevent="handleSubmit">SUBMIT</button>
-        <router-link to="/home">
-          <button class="secondaryButton">CANCEL</button>
-        </router-link>
-        <button class="resetButton" onclick="resetInputForm()" type="button">reset form</button>
-      </div>
-    <div>
-      <p>{{user}}</p>
+        <div class="buttonContainer">
+          <button class="primaryButton" id="submitButton" @click.prevent="handleSubmit">SUBMIT</button>
+          <router-link to="/home">
+            <button class="secondaryButton">CANCEL</button>
+          </router-link>
+          <button class="resetButton" onclick="resetInputForm()" type="button">reset form</button>
+        </div>
+        <div>
+          <p>{{ user }}</p>
+        </div>
+      </form>
     </div>
-    </form>
   </div>
 </template>
 
@@ -95,12 +99,12 @@
         name: 'Name',
         lat: 54,
         lng: 33,
-        title: "Title",
+        title: 'Title',
         description: 'blabla',
-        rating: "4",
+        rating: '4',
         month: 'May',
         year: '2018',
-        wiki: 'wikifiki'
+        wiki: 'wikifiki',
       };
     },
 
@@ -114,6 +118,11 @@
       handleSubmit() {
         alert('submit');
       },
+      handleLocationSubmit(event) {
+        // alert('location');
+        console.log(this.$refs.searchTextField.value);
+        this.name = this.$refs.searchTextField.value
+      },
 
       initialize() {
         const options = {
@@ -121,8 +130,7 @@
         };
         const input = document.getElementById('searchTextField');
         const autocomplete = new google.maps.places.Autocomplete(input, options);
-      }
-
+      },
     },
 
     mounted() {
@@ -131,18 +139,16 @@
         this.$router.push('/home');
       }
       this.user = this.getCurrentUser();
-      
-      this.initialize()
+
+      this.initialize();
       google.maps.event.addDomListener(window, 'load', this.initialize);
     },
-
-
   };
 </script>
 
 <style>
   #addPostForm {
-    max-width: 600px;
+    /* max-width: 600px; */
     color: #333;
     padding: 0 2rem;
   }
