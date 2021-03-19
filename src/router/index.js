3c76dd435f8d31c7d6d12365ee8f21f84e36a1df
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-03-19 14:34:47 +0100

    cleaned up store

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,11 @@
-// import { createRouter, createWebHistory } from 'vue-router';
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 
 
 import Home from '../views/Home.vue';
-// import Map from '../views/Map.vue';
-// import Map2 from '../views/Map2.vue';
-// import Map3 from '../views/Map3.vue';
-// import Map4 from '../views/Map4.vue';
 import Details from '../views/Details.vue';
 import About from '../views/About.vue';
 import NewPost from '../views/NewPost.vue';
-// import EditPost from '../views/EditPost.vue';
 import Check from '../views/Check.vue';
 import NotFound from '../views/NotFound.vue';
 import SignIn from '../views/SignIn.vue';
@@ -27,27 +21,6 @@ const routes = [
     name: 'Home',
     component: Home,
   },
-
-  // {
-  //   path: '/map',
-  //   name: 'Map',
-  //   component: Map,
-  // },
-  // {
-  //   path: '/map2',
-  //   name: 'Map2',
-  //   component: Map2,
-  // },
-  // {
-  //   path: '/map3',
-  //   name: 'Map3',
-  //   component: Map3,
-  // },
-  // {
-  //   path: '/map4',
-  //   name: 'Map4',
-  //   component: Map4,
-  // },
   {
     path: '/new',
     name: 'NewPost',
@@ -59,12 +32,6 @@ const routes = [
     component: Details,
     props: true,
   },
-  // {
-  //   path: '/edit/:id',
-  //   name: 'Edit',
-  //   component: EditPost,
-  //   props: true,
-  // },
   {
     path: '/check',
     name: 'Check',
@@ -93,10 +60,6 @@ const routes = [
   },
 ];
 
-// const router = createRouter({
-//   history: createWebHistory(process.env.BASE_URL),
-//   routes,
-// });
 
 const router = createRouter({
   history: createWebHashHistory(),
