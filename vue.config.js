4505da0bbf4795608c731ebef1f747df37a4f945
Steffen Häfelinger <s.haefelinger@gmx.de>
2021-12-29 11:59:45 +0100

    removed dev server

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  devServer: {
-    proxy: {
-      '^/auth': {
-        target: 'http://localhost:3000',
-        // target: 'https://aroundtheworld-blog-server.herokuapp.com',
-        // target: 'https://aroundtheworld-backend2.apps.functionfactory.de',
-        changeOrigin: true,
-      },
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '^/auth': {
+  //       target: 'http://localhost:3000',
+  //       // target: 'https://aroundtheworld-blog-server.herokuapp.com',
+  //       // target: 'https://aroundtheworld-backend2.apps.functionfactory.de',
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 };
